21629b19ef810920b78d86d76b8c5c903a5441ef
Léo <58048782+ZenT0x@users.noreply.github.com>
2024-03-26 12:08:08 +0100

    Enter key for submit post and it's working

--- js/home.js
+++ js/home.js
@@ -1,4 +1,30 @@
 document.querySelector('.buttonWritePost').addEventListener('click', function() {
     document.getElementById('modalPost').style.display = 'block';
 });
-    
+
+$(document).ready(function(){
+    $('.textAreaPost').on('keydown', function(e) {
+        // Si la touche pressée est Entrée
+        if (e.which == 13) {
+            e.preventDefault();
+            $('#formPostId').submit();
+        }
+    });
+
+    $('#formPostId').submit(function(e){
+        e.preventDefault(); // Empêcher le formulaire de se soumettre normalement
+        
+        // Récupérer les données du formulaire
+        var formData = $(this).serialize();
+        // Envoyer les données du formulaire via AJAX
+        $.ajax({
+            type: 'POST',
+            url: 'php_tool/submit_post.php',
+            data: formData,
+            success: function(response){
+                document.getElementById('modalPost').style.display = 'none';
+                document.getElementById('formPostId').reset();
+            }
+        });
+    });
+});
