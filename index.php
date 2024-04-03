45d1b46be16727ad99caaf043b72bb5d2afe0d40
Hugo Allainé <hugo.allaine@gmail.com>
2024-04-03 10:18:48 +0200

    fix bootstrap 2

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ require_once dirname(__FILE__).'/php_tool/db.php';
 <html lang='fr'>
 <head>
     <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>YGreg - Home</title>
     <link rel="icon" type="image/png" href="img/logo/YGreg_logo.png"/>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' href='css/default.css'>
     <link rel='stylesheet' type='text/css' href='css/home.css'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
