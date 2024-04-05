2e413db1488fa505a365f6004b4472e6414cba59
Hugo Allainé <hugo.allaine@gmail.com>
2024-04-05 15:40:09 +0200

    début d'un template

--- profile.php
+++ profile.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once dirname(__FILE__).'/php_tool/db.php';
+require_once dirname(__FILE__) . '/php_tool/db.php';
 
 
 ?>
@@ -12,47 +12,96 @@ require_once dirname(__FILE__).'/php_tool/db.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>YGreg - Profil</title>
     <link rel="icon" type="image/png" href="img/logo/YGreg_logo.png" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-2">
-                <nav class="navbar bg-body-tertiary">
-                    <div class="container-fluid">
-                        <img src="img/logo/YGreg_logo.png" alt="YGreg logo" class="img-fluid">
-                        <h1 class="navbar-brand">YGreg</h1>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav flex-column mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" href="#">Se connecter</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">S'inscrire</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
+            <div class="col-2 p-0 vh-100">
+                <div class="d-flex flex-column flex-shrink-0 p-3 bg-light h-100">
+                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                        <img src="img/logo/YGreg_logo.png" alt="YGreg" width="40" height="40"
+                            class="bi me-2">
+                        <span class="fs-4">YGreg</span>
+                    </a>
+                    <hr>
+                    <ul class="nav nav-pills flex-column mb-auto">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link link-dark">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#home" />
+                                </svg>
+                                Accueil
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link active" aria-current="page">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#speedometer2" />
+                                </svg>
+                                Profil
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link link-dark">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#table" />
+                                </svg>
+                                Messages
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link link-dark">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#grid" />
+                                </svg>
+                                Amis
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link link-dark">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#people-circle" />
+                                </svg>
+                                ???
+                            </a>
+                        </li>
+                    </ul>
+                    <hr>
+                    <div class="dropdown">
+                        <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
+                            id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                            <strong>Pseudo</strong>
+                        </a>
+                        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                            <li><a class="dropdown-item" href="#">Paramètres</a></li>
+                            <li><a class="dropdown-item" href="#">Support</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Se déconnecter</a></li>
+                        </ul>
                     </div>
-                </nav>
+                </div>
             </div>
-            <div class="col-10">
+            <div class="col-10 p-0">
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container-fluid d-flex justify-content-center">
+                    <div class="container-fluid d-flex justify-content-space-around">
                         <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit"><img src="img/icon/loupe.png" class="img-fluid d-block mx-auto" width="30"></button>
+                            <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit"><img src="img/icon/loupe.png"
+                                    class="img-fluid d-block mx-auto" width="30"></button>
                         </form>
+                        <button class="btn btn-primary " type="button">Ecrire un tweet</button>
                     </div>
                 </nav>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 </html>
