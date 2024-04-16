638204bcaa679a5c2c6657ffc723dac503bdfec8
HugoPlayer <hugo+github@allaine.cc>
2024-04-16 16:57:15 +0200

    Ajouter la fonction isTfaEnabled pour vérifier si l'authentification à deux facteurs est activée + Ajout fonction session_start_secure() pour démarrer la session sans erreur

--- php_tool/alreadyConnected.php
+++ php_tool/alreadyConnected.php
@@ -1,6 +1,14 @@
 <?php
 require_once dirname(__FILE__).'/db.php';
 
+function session_start_secure() {
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+}
+
+session_start_secure();
+
 function isConnected() {
     global $db;
     if (isset($_SESSION['token']) && isset($_SESSION['id'])) {
@@ -14,15 +22,26 @@ function isConnected() {
     return false;
 }
 
-function redirectIfConnected($url = "index.php") {
+function redirectIfConnected($url = "profile.php") {
     if (isConnected()) {
         header("Location: $url");
     }
 }
 
-function redirectIfNotConnected($url = "login.php") {
+function redirectIfNotConnected($url = "index.php") {
     if (!isConnected()) {
         header("Location: $url");
+    }
+}
+
+function isTfaEnabled() {
+    global $db;
+    $req = $db->prepare("SELECT tfaKey FROM users WHERE id = ? AND tfaKey IS NULL");
+    $req->execute(array($_SESSION['id']));
+    if ($req->rowCount() == 0) {
+        return true;
+    } else {
+        return false;
     }
 }
 ?>
