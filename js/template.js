e8f577f72a4a4d27a345a14730fb3ca75cebe6c3
?FpAdxF&ci4Xpe <zentoxll@gmail.com>
2024-04-18 23:04:25 +0200

    Ajout de fonctionnalités pour les boutons de modal et de soumission de formulaire dans index.js

--- js/template.js
+++ js/template.js
@@ -1,13 +1,3 @@
-var buttons = document.querySelectorAll('[data-bs-toggle="modal"][data-bs-target="#modalPost"]');
-
-buttons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        var tweetId = button.getAttribute('data-tweet-id');
-        var input = document.querySelector('input[name="id_parent"]');
-        input.value = tweetId;
-    });
-});
-
 function TogglePassword(checked) {
     var x = document.getElementById("password");
     if (checked) {
@@ -30,27 +20,6 @@ function TogglePasswordRegister(checked) {
 }
 
 $(document).ready(function () {
-    $('.textAreaPost').on('keydown', function (e) {
-        if (e.which == 13) {
-            e.preventDefault();
-            $('#formPostId').submit();
-        }
-    });
-
-    /* Envoi du formulaire d'ajout de post */
-    $('#formPostId').submit(function (e) {
-        e.preventDefault();
-        var formData = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: 'php_tool/submit_post.php',
-            data: formData,
-            success: function (response) {
-                document.getElementById('formPostId').reset();
-                $('#modalPost').modal('hide');
-            }
-        });
-    });
 
     /* Login */
     $('#formLoginId').submit(function (e) {
@@ -115,23 +84,5 @@ $(document).ready(function () {
         });
     });
 
-    /* Like post */
-    $('.like-button').click(function () {
-        var postId = $(this).data('post-id');
-        var likeImage = $(this).find('img');
-        $.ajax({
-            url: 'php_tool/like_post.php',
-            type: 'POST',
-            data: {
-                post_id: postId
-            },
-            success: function (response) {
-                if (response === 'liked') {
-                    likeImage.attr('src', '/WE4A_project/img/icon/liked.png');
-                } else {
-                    likeImage.attr('src', '/WE4A_project/img/icon/like.png');
-                }
-            }
-        });
-    });
+
 });
