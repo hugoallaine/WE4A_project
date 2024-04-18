c382c0b2966c8e608de35cd9060ff37e4af1f385
?FpAdxF&ci4Xpe <zentoxll@gmail.com>
2024-04-18 19:15:11 +0200

    Correction de la fonction sendPost pour gérer le cas où parentId est null

--- php_tool/submit_post.php
+++ php_tool/submit_post.php
@@ -4,19 +4,30 @@ session_start_secure();
 
 require_once dirname(__FILE__).'/db.php';
 
-function sendPost($post, $parentId) {
+function sendPost($post, $parentId = null) {
     global $db;
-    $req = $db->prepare("INSERT INTO posts (id_user, content, id_parent) VALUES (?, ?, ?)");
-    $req->execute(array($_SESSION['id'], $post, $parentId));
+    if ($parentId !== null) {
+        $req = $db->prepare("INSERT INTO posts (id_user, content, id_parent) VALUES (?, ?, ?)");
+        $req->execute(array($_SESSION['id'], $post, $parentId));
+    } else {
+        $req = $db->prepare("INSERT INTO posts (id_user, content) VALUES (?, ?)");
+        $req->execute(array($_SESSION['id'], $post));
+    }
 }
 
 if (isset($_POST['textAreaPostId'])) {
     $post = SecurizeString_ForSQL($_POST['textAreaPostId']);
-    $parentId = isset($_POST['id_parent']) ? SecurizeString_ForSQL($_POST['id_parent']) : NULL;
+    
     if (!empty($post)) {
+        if (isset($_POST['id_parent']) && !empty($_POST['id_parent'])) {
+            $parentId = SecurizeString_ForSQL($_POST['id_parent']);
+        } else {
+            $parentId = null;
+        }
         sendPost($post, $parentId);
     } else {
         $error = "Le message est vide";
     }
 }
+
 ?>
