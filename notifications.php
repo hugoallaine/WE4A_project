6600374f69cbcd699ca14e24be5effbd2f773b11
Hugo Allainé <hugo+github@allaine.cc>
2024-04-24 11:56:45 +0200

    tentative de correction affichage posts sur la hauteur + fix affichage image post avec lien

--- notifications.php
+++ notifications.php
@@ -12,7 +12,7 @@ $currentPage = 'Notifications';
 require_once dirname(__FILE__).'/php_tool/template_top.php';
 ?>
 <main>
-    <div class="notifications-container col-12 p-0 overflow-auto vh-100 d-flex align-items-center flex-column">
+    <div class="notifications-container col-12 p-0 overflow-auto h-100 d-flex align-items-center flex-column">
         <h1 class="text-primary p-3">Notifications</h1>
         <?php
             foreach ($notifications as $notification) {
--- php_tool/template_top.php
+++ php_tool/template_top.php
@@ -60,7 +60,7 @@ require_once dirname(__FILE__).'/toast.php';
                     <div class="dropdown">
                         <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
                             id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="<?php if(!empty($_SESSION['avatar'])){echo "img/user/".$_SESSION['id']."/".$_SESSION['avatar'];}else{echo "img/icon/utilisateur.png";} ?>" alt="" width="32" height="32" class="rounded-circle me-3" style="object-fit: cover;">
+                            <img src="<?php if(!empty($_SESSION['avatar'])){echo "img/user/".$_SESSION['id']."/".$_SESSION['avatar'];}else{echo "img/icon/utilisateur.png";} ?>" alt="Icon User" width="32" height="32" class="rounded-circle me-3" style="object-fit: cover;">
                             <strong><?php echo $_SESSION['pseudo']; ?></strong>
                         </a>
                         <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
@@ -74,7 +74,7 @@ require_once dirname(__FILE__).'/toast.php';
                     </div>
                     <?php else: ?>
                     <a href="#" id="login" class="d-flex align-items-center link-dark text-decoration-none" data-bs-toggle='modal' data-bs-target='#modalLogin'>
-                        <img src="img/icon/utilisateur.png" alt="" width="32" height="32" class="rounded-circle me-3">
+                        <img src="img/icon/utilisateur.png" alt="Icon User" width="32" height="32" class="rounded-circle me-3">
                         <strong>Se connecter</strong>
                     </a>
                     <?php endif; ?>
