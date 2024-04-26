782808a599b33681ecf605ba7c8e8f5334f66316
Hugo Allainé <hugo+github@allaine.cc>
2024-04-26 15:52:08 +0200

    parser fait pour post

--- php_tool/parser.php
+++ php_tool/parser.php
@@ -1,11 +1,19 @@
 <?php
+require_once dirname(__FILE__).'/db.php';
 
 function parsePseudoForProfile($string) {
+    global $db;
     $pattern = '/@(\w+)/';
     
-    $parsedString = preg_replace_callback($pattern, function($matches) {
+    $parsedString = preg_replace_callback($pattern, function($matches,$db) {
         $username = $matches[1];
-        return '<a href="profile.php?pseudo='.$username.'" class="link-primary link-underline link-underline-opacity-0 link-underline-opacity-75-hover">'.$username.'</a>';
+        $req = $db->prepare("SELECT * FROM users WHERE pseudo = ?");
+        $req->execute(array($username));
+        $exist = $req->rowCount();
+        if ($exist == 1) {
+            return '<a href="profile.php?pseudo='.$username.'" class="link-primary link-underline link-underline-opacity-0 link-underline-opacity-75-hover">'.$username.'</a>';
+        }
+        return "@".$username;
     }, $string);
     
     return $parsedString;
