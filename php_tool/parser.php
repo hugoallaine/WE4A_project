ac3818ce1b0c1a4e437b921c8f7f2e25e7efaf40
Hugo Allainé <hugo+github@allaine.cc>
2024-04-26 16:25:29 +0200

    fix notification

--- php_tool/parser.php
+++ php_tool/parser.php
@@ -5,10 +5,10 @@ function parsePseudoForProfile($string) {
     global $db;
     $pattern = '/@(\w+)/';
     
-    $parsedString = preg_replace_callback($pattern, function($matches,$db) {
+    $parsedString = preg_replace_callback($pattern, function($matches) use ($db) {
         $username = $matches[1];
-        $req = $db->prepare("SELECT * FROM users WHERE pseudo = ?");
-        $req->execute(array($username));
+        $req = $db->prepare('SELECT * FROM users WHERE pseudo = ?');
+        $req->execute([$username]);
         $exist = $req->rowCount();
         if ($exist == 1) {
             return '<a href="profile.php?pseudo='.$username.'" class="link-primary link-underline link-underline-opacity-0 link-underline-opacity-75-hover">'.$username.'</a>';
