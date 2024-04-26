4cf03f5ead450db030ae3a948559bffd8244f32c
Hugo Allainé <hugo+github@allaine.cc>
2024-04-26 17:43:17 +0200

    Fix empty notifications display issue

--- notifications.php
+++ notifications.php
@@ -15,6 +15,7 @@ require_once dirname(__FILE__).'/php_tool/template_top.php';
     <div class="notifications-container col-12 p-0 overflow-auto h-100 d-flex align-items-center flex-column">
         <h1 class="text-primary p-3">Notifications</h1>
         <?php
+        if (!empty($notifications)) {
             foreach ($notifications as $notification) {
                 $date = date_create_from_format('Y-m-d H:i:s', $notification['created_at']);
                 $formatted_date = $date->format('d/m/Y H:i:s');
@@ -40,6 +41,11 @@ require_once dirname(__FILE__).'/php_tool/template_top.php';
                         </div>
                     </div>";
             }
+        } else {
+            echo "<div class='d-flex justify-content-center align-items-center h-75'>
+                    <h2 class='text-center'>Aucune notification</h2>
+                </div>";
+        }
         ?>
     </div>
 </main>
