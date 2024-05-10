be4a34610aa8c605698936ef2aa2bb10dbb5daa7
HugoPlayer <hugo+github@allaine.cc>
2024-05-10 21:37:06 +0200

    fix icon default dark theme

--- php/template_top.php
+++ php/template_top.php
@@ -61,7 +61,7 @@ require_once dirname(__FILE__).'/toast.php';
                         <?php if(isConnected()): ?>
                         <div class="dropdown">
                             <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="<?php if(!empty($_SESSION['avatar'])){echo "img/user/".$_SESSION['id']."/".$_SESSION['avatar'];}else{echo "img/icon/utilisateur.png";} ?>" alt="Icon User" width="40" height="40" class="rounded-circle me-3 object-fit-cover">
+                                <img src="<?php if(!empty($_SESSION['avatar'])){echo "img/user/".$_SESSION['id']."/".$_SESSION['avatar'];}else{echo "img/icon/utilisateur.png";} ?>" alt="Icon User" width="40" height="40" class="<?php if(empty($_SESSION['avatar'])){echo 'icon';} ?> rounded-circle me-3 object-fit-cover">
                                 <strong class="fs-5"><?php echo $_SESSION['pseudo']; ?></strong>
                             </a>
                             <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
